8cfa6cd8073b93aa3bd759e0f42066becfb762d9
wilrel <wildannasrulloh321@gmail.com>
2021-12-29 14:10:04 +0700

    first commit

---
+++ js/ngeri.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+    $("a").on('click', function(event) {
+
+
+      if (this.hash !== "") {
+
+        event.preventDefault();
+
+
+        var hash = this.hash;
+
+
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top-50
+        }, 800, function(){
+
+
+          window.location.hash = hash-50;
+        });
+      } 
+    });
+});
+
+
+
+
